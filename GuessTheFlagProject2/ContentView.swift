6aea85c7636150fb70e0ca313f5aa511077d4503
Ami <foreverazo2013@gmail.com>
2019-11-05 12:00:05 +0100

    My first App with flag of countries

--- GuessTheFlagProject2/ContentView.swift
+++ GuessTheFlagProject2/ContentView.swift
@@ -9,8 +9,54 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia" ,"Spain" ,"UK", "US"]
+        .shuffled()
+    // Para que sirve el shuffled:
+    @State private var correctAnswer = Int.random(in: 0...2)
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    
     var body: some View {
-        Text("Hello World")
+        ZStack{
+            Color.red.edgesIgnoringSafeArea(.all)
+            VStack(spacing: 30){
+                VStack {
+                    Text("Tap the flag of").foregroundColor(.white)
+                    Text(countries[correctAnswer]).foregroundColor(Color.white)
+                }
+                ForEach(0 ..< 3){ number in
+                    Button(action: {
+                        self.flagTapped(number)
+                    }){
+                        Image(self.countries[number])
+                            .renderingMode(.original)
+                    }
+                    
+                }
+                Spacer()
+            }
+        }.alert(isPresented: $showingScore){
+            Alert(title: Text(scoreTitle), message: Text("Your score is ??"), dismissButton: .default(Text("Continue")){
+                
+                self.askQuestion()
+            })
+        }
+    }
+    
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+        } else {
+            scoreTitle = "Wrong"
+        }
+
+        showingScore = true
+    }
+    
+    func askQuestion(){
+        countries.shuffled()
+        correctAnswer = Int.random(in: 0...2)
     }
 }
 
